90a5b12102b5ecd1b1cb9c1c3712113aea26da49
Leo Roderick <leo@liontude.com>
2025-02-07 13:05:46 -0500

    removing folder

--- geira_icons/example/android/app/src/main/kotlin/com/example/geira_icons_example/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.geira_icons_example
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- geira_icons/lib/geira_icons.dart
+++
@@ -1,8 +0,0 @@
-
-import 'geira_icons_platform_interface.dart';
-
-class GeiraIcons {
-  Future<String?> getPlatformVersion() {
-    return GeiraIconsPlatform.instance.getPlatformVersion();
-  }
-}
